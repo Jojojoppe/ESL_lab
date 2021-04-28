c5c5d58b5112ddc5693377dcc59639cd6a2de957
Joppe Blondel <joppe@blondel.nl>
2021-04-28 13:47:21 +0200

    New NIOS system

---
+++ fpga_ip_demo/software/fpga_ip_demo_main/obj/default/main.d
@@ -0,0 +1,11 @@
+obj/default/main.o: main.c ../fpga_ip_demo_main_bsp//HAL/inc/io.h \
+ ../fpga_ip_demo_main_bsp//HAL/inc/alt_types.h \
+ ../fpga_ip_demo_main_bsp/system.h ../fpga_ip_demo_main_bsp/linker.h
+
+../fpga_ip_demo_main_bsp//HAL/inc/io.h:
+
+../fpga_ip_demo_main_bsp//HAL/inc/alt_types.h:
+
+../fpga_ip_demo_main_bsp/system.h:
+
+../fpga_ip_demo_main_bsp/linker.h:
